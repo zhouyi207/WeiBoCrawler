caeda843d74f47a7f62ba813dac3739c253f1b36
zy <zhouyi_207@foxmail.com>
2025-01-30 22:19:14 +0800

    测试更新

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 # from WeiBoCrawler.pack.get_list_data import get_list_data
 
-# get_list_data("#姜萍已达到数学系本科生的水平#")
+# get_list_data("#姜萍已达到数学系本科生的水平#", table_name="test")
 
 # **************************************************************************
 
@@ -9,13 +9,13 @@
 
 # ids = ['OEEV7wXHY', 'Oj0PXme8I', 'OiZre8dir', 'Oj0zUmucE', 'Oj00ybFds', 'Oj18tlqkf', 'Oj0Bg4v3d', 'Oj45yl6ur', 'Oj4HO1IrL', 'Oj5cxg2BF', 'Oj0O9uqNY', 'Oj8diA5Cy', 'Ojfeevno5', 'Oj8NcmH46', 'OFrC10SjD', 'OF7yZin9y', 'OENCBx1d3', 'OEN6K6UZi', 'OEHVExfdF', 'OEHAiaNmj', 'OEyCC6XUU', 'OEHda68aH', 'OEGnSjKz9', 'OEEbgiue6', 'OEBxCkT93', 'OEB6c386R', 'OEB3ubAld', 'OEzx35ziq', 'OExXyvm9u', 'OExSO0NwR', 'OExDS362r', 'OEx5RuzM1', 'OEwW7jk3z', 'OEwSKABQT', 'OEwFHtTxD', 'OEwFBhFEe', 'OEwEupvF7', 'OEwu4sLb3', 'OEws2De8S', 'OD2LH5AS4', 'OALhwgk3L', 'OzSIitvQX', 'Ovorck7Nx', 'OsPxkawcf', 'OpdKGh5Ue', 'OnS1itKvR', 'OmDhFdd4b', 'Om97njrjv', 'OlhVmcko6', 'OleJWjWMV', 'Ol7KvipSZ', 'Ol7bJlzdm', 'OkYKjxbX2', 'OkYlJCENy', 'OkXgvcUq0', 'OkVbOwnkz', 'OkCYwst1P', 'OkBtdCBZj', 'Oks8yDEsL', 'OknS6nZTZ', 'OkjNc3Gah', 'Okh1b1YeY', 'OkdDViiDG', 'OkcNz0LQe', 'OkbUZAmjo', 'Ok9EgljZd', 'Ok82qByPR', 'Ok5QqmsgR', 'Ok4J4eKAy', 'Ok2iMroBg', 'OjYT5fin7', 'OjYD7fQJN', 'OjVJ5aUzI', 'OjY3XghTF', 'OjRPDFViM', 'OjRzT67HP', 'OjQwT0WTN', 'OjQuR70on', 'OjKaZ3xJU', 'OjPL2Acx4', 'OjM7f2CVM', 'OjLGuv2qw', 'OjKNA63P8', 'OjJXcEzus', 'OjJHebrm6', 'OjJvExZNw', 'OjJfBa3Cb', 'OjHdhpy8V', 'OjGbo4PZi', 'OjG3wx7uV', 'OjtyiCIW7', 'OjziZ4jD4', 'Ojzibwc0C', 'OjyUnqC55', 'OjyRUhf2n', 'OjyfHmWpR', 'OjxPnlGxN', 'OjxIw0cSp', 'OjwzsFvaw', 'Ojw4IsrQh', 'OjvBGAKOr', 'OjlOb6KAv', 'Ojm6NzMsI', 'Ojs5Ldnu0', 'Ojs2o3coe', 'OjrpZfcLW', 'OjqiapmXz', 'OjpgEFE0n', 'Ojpai3283', 'Ojp0NAE5z', 'OjoZ55om4', 'OjoIOykNV', 'OjoHWqxBx', 'OjoD3jqp5', 'OjoAVgzKz', 'OjosMdwhJ', 'OjokJii0Y', 'Ojo1jC2lB', 'OjnZgwanP', 'OjnrokMuQ', 'OjnjGdmiv', 'OjnaPBdp3', 'OjmPzicwW', 'OjlXhv4a2', 'OjjWW6TbH', 'OjjE0yZXR', 'Ojjr8l0FV', 'OjjdBwlzN', 'Ojj3a0zpU', 'Ojj1hnRMA', 'OjiZH1dhm', 'OjiSrrLeZ', 'Oji1y1hrp', 'Oj9hu7h81', 'OjhYrvneG', 'OjhDbCLLy', 'Ojhyvz2iN', 'Ojheh6MKh', 'OjgGwiroo', 'OjgeDbYDl', 'Ojg9bidJz', 'OjfXAnmIP', 'OjfMLz2tB', 'OjfI2BjLS', 'OjfoxEgnx', 'Oj9Zk2nqW', 'OjeOVney8', 'OjeKPgZB3', 'OjeDCuYsT', 'OjeB5rLaO', 'OjeqnlMZF', 'Ojeg9aZnz', 'OjeeYsiPw', 'Oje9eofv7', 'Oje7m4vHh', 'Oje2suyPz', 'Oje1u7fVH', 'OjdGzclyZ', 'OjdDcpe7F', 'OjdeXp6SV', 'Ojba1A0or', 'OjaEV6HyU', 'Ojabj6xiq', 'Oja7xsXvq', 'Oj9ZydhY0', 'Oj9Upy4EC', 'Oj9TafHKn', 'Oj9NsF2Y9', 'Oj9LYmVwb', 'Oj9JymYjq', 'Oj9HwF803', 'Oj9xSaefw', 'Oj9xJyLXt', 'Oj9vW26QJ', 'Oj57h8fwm', 'Oj9bVDWzq', 'Oj97fwWOU', 'Oj8Yb3TCr', 'Oj8Q7gwJf', 'Oj8LuAxpG', 'Oj8tNjc8d', 'Oj8dED6gb', 'Oj85Q9ypg', 'Oj82buRAs', 'Oj7Wo369A', 'Oj7WbzZrQ', 'Oj7bIjXTE', 'Oj7P2gjL6', 'Oj7Nx2Klc', 'Oj7nKoLS4', 'Oj7kHfIEc', 'Oj7aU3QXO', 'Oj7a69JWD', 'Oj6WFypNW', 'Oj6Kp6zDF', 'Oj6uTgU7C', 'Oj6lg5Cxr', 'Oj6g6qyeA', 'Oj64WiwNu', 'Oj5Q3nF6a', 'Oj5OZsOr2', 'Oj5zNjpPx', 'Oj5wwfWtl', 'Oj5wrxfm2', 'Oj5nCmFAk', 'Oj5lBBnIt', 'Oj5gpdxhu', 'Oj5e3xPZk', 'Oj5d22f3O', 'Oj57njnAI', 'Oj54vlKYM', 'Oj0v9mSSd', 'Oj00Jpihm']
 
-# get_body_data(ids)
+# get_body_data(ids, table_name="test")
 
 # **************************************************************************
 
 # from WeiBoCrawler.pack.get_comment1_data import get_comment1_data
 
-# get_comment1_data(uid=["2035895904", "1749277070", "1644114654"], mid=["5096904217856018", "5045463240409185","5045260761957739"], asynchrony=True)
+# get_comment1_data(uid=["2035895904", "1749277070", "1644114654"], mid=["5096904217856018", "5045463240409185","5045260761957739"], table_name="test")
 
 
 # **************************************************************************
@@ -23,6 +23,7 @@
 
 # from WeiBoCrawler.pack.get_comment2_data import get_comment2_data
 
-# get_comment2_data(uid=["1644114654", "1644114654", "1644114654"], mid=["5045280045531535", "5045270515551948","5045277713760776"], asynchrony=True)
+# get_comment2_data(uid=["1644114654", "1644114654", "1644114654"], mid=["5045280045531535", "5045270515551948","5045277713760776"], table_name="test")
 
 # **************************************************************************
+
